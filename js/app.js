2cb297c35f936b153fcae113f1ddf338e813d509
Oli Forster <oliforster798@gmail.com>
2016-01-29 11:54:17 +0000

    2 players 2keys to move

--- js/app.js
+++ js/app.js
@@ -2,26 +2,58 @@ $(function (){
   console.log("loaded");
 
 
-  $(window).keyup(function(event) {
-  
-    if (event.which === 90) {
-      console.log("click");
-      $('.box').animate({
-        'marginTop' : "-=4px" 
-      }, 1)
-    }
 
-    });
+
+
+
+  var clickCount =1
+  var clickCount2 =1
 
   $(window).keyup(function(event) {
-  
-    if (event.which === 77) {
-      console.log("click2");
-      $('.box2').animate({
-        'marginTop' : "-=4px" 
-      }, 1)
+    console.log(event.which)
+
+    if (clickCount % 2 === 0) {
+
+      if (event.which === 90) {
+        console.log("click");
+        $('.box').animate({
+          'marginTop' : "-=4px"  
+        }, 1);
+        clickCount +=1;;
+        console.log(clickCount)
+      }
+    } else if (clickCount % 2 !== 0) {
+      if (event.which === 88) {
+        console.log("click");
+        $('.box').animate({
+          'marginTop' : "-=4px"
+        }, 1);
+        clickCount +=1; 
+        console.log(clickCount)
+      }
     }
+  });
 
-    });
+  $(window).keyup(function(event) {
 
+
+    if (clickCount2 % 2 === 0) {
+
+      if (event.which === 75) {
+        console.log("click");
+        $('.box2').animate({
+          'marginTop' : "-=4px"  
+        }, 1);
+        clickCount2 +=1;;
+      }
+    } else if (clickCount2 % 2 !== 0) {
+      if (event.which === 76) {
+        console.log("click");
+        $('.box2').animate({
+          'marginTop' : "-=4px"
+        }, 1);
+        clickCount2 +=1; 
+      }
+    }
+  });
 });
