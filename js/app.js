8ed0b5e1dbc9b850cbd4568584db898959eb768d
Oli Forster <oliforster798@gmail.com>
2016-01-29 16:18:44 +0000

    working win logic

--- js/app.js
+++ js/app.js
@@ -30,7 +30,25 @@ var winner2 = win(box2Pos, finishPos)
 
 
 
+              // overall win logic
+var overallWinner = 0
 
+function overallWin (box1Pos, box2Pos){
+  var finishPos = $('.finish').offset().left;
+  if (box1Pos > finishPos) {
+    console.log("P1 wins");
+    return overallWinner = "Player 1 wins";
+  } else if (box2Pos > finishPos) {
+    console.log("P1 wins");
+    return overallWinner = "player 2 wins"
+  } else {
+    console.log("No winner");
+    // return false;
+
+  }
+}
+
+// var overallWinner = overallWin(box1Pos, box2Pos)
 
   var clickCount =1
   var clickCount2 =1
@@ -39,48 +57,58 @@ var winner2 = win(box2Pos, finishPos)
 //red box
   $(window).keyup(function(event) {
 
-    if (clickCount % 2 === 0) {
+    if (overallWinner === 0){
 
-      if (event.which === 90) {
-        $box1.css({ left: "+=40px" });
-        clickCount +=1;
-        box1Pos = $box1.offset().left + $box1.width();
-        win(box1Pos, finishPos)
-        
-      }
-    } else if (clickCount % 2 !== 0) {
-      if (event.which === 88) {
-        $box1.css({ left: "+=40px" });
-        clickCount +=1; 
-        box1Pos = $box1.offset().left + $box1.width();
-        win(box1Pos, finishPos)
-      }
+     if (clickCount % 2 === 0) {
+
+       if (event.which === 90) {
+         $box1.css({ left: "+=40px" });
+         clickCount +=1;
+         box1Pos = $box1.offset().left + $box1.width();
+        // win(box1Pos, finishPos);
+         overallWin(box1Pos, box2Pos);
+         
+       }
+     } else if (clickCount % 2 !== 0) {
+       if (event.which === 88) {
+         $box1.css({ left: "+=40px" });
+         clickCount +=1; 
+         box1Pos = $box1.offset().left + $box1.width();
+       //  win(box1Pos, finishPos);
+         overallWin(box1Pos, box2Pos);
+       }
+     }
+    } else {
+      console.log(overallWinner)
     }
   });
 
 //blue box
   $(window).keyup(function(event) {
 
-
-    if (clickCount2 % 2 === 0) {
-
-      if (event.which === 75) {
-        $box2.animate({
-          'margin-left' : "+=40px"  
-        }, 1);
-        clickCount2 +=1;; 
-        box2Pos = $box2.offset().left + $box2.width();
-        win2(box2Pos, finishPos)
-      }
-    } else if (clickCount2 % 2 !== 0) {
-      if (event.which === 76) {
-        $box2.animate({
-          'margin-left' : "+=40px"
-        }, 1);
-        clickCount2 +=1; 
-        box2Pos = $box2.offset().left + $box2.width();
-        win2(box2Pos, finishPos)
-      }
+    if (overallWinner === 0){
+     if (clickCount2 % 2 === 0) {
+  
+       if (event.which === 75) {
+         $box2.animate({
+           'margin-left' : "+=40px"  
+         }, 1);
+         clickCount2 +=1;; 
+         box2Pos = $box2.offset().left + $box2.width();
+         overallWin(box1Pos, box2Pos);
+       }
+     } else if (clickCount2 % 2 !== 0) {
+       if (event.which === 76) {
+         $box2.animate({
+           'margin-left' : "+=40px"
+         }, 1);
+         clickCount2 +=1; 
+         box2Pos = $box2.offset().left + $box2.width();
+         overallWin(box1Pos, box2Pos);
+       }
+     }
+    }else {
+      console.log(overallWinner)
     }
   });
 });
