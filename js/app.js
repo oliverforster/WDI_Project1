ec32d79d250773d09139c68887b4b9c3c24cd827
Oli Forster <oliforster798@gmail.com>
2016-01-29 14:09:52 +0000

    starting win logic

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 $(function (){
   console.log("loaded");
 
+var boxLeft = $('.box').offset().left;
+console.log(boxLeft);
+var boxWidth = $('.box').outerWidth(true);
+console.log(boxWidth);
+var box1Pos = boxLeft + boxWidth;
+console.log(box1Pos)
+
+
+var finishLeft = $('.finish').offset().left;
+var finishPos = finishLeft;
+console.log(finishPos)
+
+function win(box1Pos, finishPos){
+  if(box1Pos > finishPos) return true; return false
+}
+
+var winner = win()
+console.log(winner)
 
 
 
@@ -9,31 +27,34 @@ $(function (){
   var clickCount =1
   var clickCount2 =1
 
+  
+//red box
   $(window).keyup(function(event) {
     console.log(event.which)
 
     if (clickCount % 2 === 0) {
 
       if (event.which === 90) {
-        console.log("click");
         $('.box').animate({
-          'marginTop' : "-=4px"  
+         'margin-left' : "+=4px"  
         }, 1);
         clickCount +=1;;
-        console.log(clickCount)
+        win()
+        console.log(winner);
       }
     } else if (clickCount % 2 !== 0) {
       if (event.which === 88) {
-        console.log("click");
         $('.box').animate({
-          'marginTop' : "-=4px"
+          'margin-left' : "+=4px"
         }, 1);
         clickCount +=1; 
-        console.log(clickCount)
+        win()
+        console.log(winner);
       }
     }
   });
 
+//blue box
   $(window).keyup(function(event) {
 
 
@@ -42,7 +63,7 @@ $(function (){
       if (event.which === 75) {
         console.log("click");
         $('.box2').animate({
-          'marginTop' : "-=4px"  
+          'margin-left' : "+=4px"  
         }, 1);
         clickCount2 +=1;;
       }
@@ -50,10 +71,13 @@ $(function (){
       if (event.which === 76) {
         console.log("click");
         $('.box2').animate({
-          'marginTop' : "-=4px"
+          'margin-left' : "+=4px"
         }, 1);
         clickCount2 +=1; 
       }
     }
   });
 });
+
+
+
